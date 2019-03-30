a8c1362acf0a8847bc4b3091dd2136257490bf07
devesh <deveshkailay24@gmail.com>
2019-03-30 14:06:22 +0530

    second commit

---
+++ warehousemgmt/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+	path('',views.home,name="home"),
+	path('warehouse_signup',views.warehouse_signup,name="warehouse_signup"),
+	path('shop_signup',views.shop_signup,name='shop_signup')
+]
--- warehousemgmt/views.py
+++ warehousemgmt/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse, HttpResponseRedirect
 # Create your views here.
+
+def home(request):
+	return render(request,'home.html')
+
+def warehouse_signup(request):
+	return render(request,'warehouse_signup.html')
+
+def shop_signup(request):
+	return render(request,'shop_signup.html')
