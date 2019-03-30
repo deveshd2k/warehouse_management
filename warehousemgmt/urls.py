f61baacb5dc3df1bcfc58c3f33a68c2be977f087
devesh <deveshkailay24@gmail.com>
2019-03-30 17:42:13 +0530

    third commit

--- warehousemgmt/urls.py
+++ warehousemgmt/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
 	path('',views.home,name="home"),
 	path('warehouse_signup',views.warehouse_signup,name="warehouse_signup"),
-	path('shop_signup',views.shop_signup,name='shop_signup')
+	path('shop_signup',views.shop_signup,name='shop_signup'),
+	path('login',views.login,name="login"),
+	path('dashboard',views.dashboard,name="dashboard")
 ]
--- warehousemgmt/views.py
+++ warehousemgmt/views.py
@@ -10,3 +10,9 @@ def warehouse_signup(request):
 
 def shop_signup(request):
 	return render(request,'shop_signup.html')
+
+def login(request):
+	return render(request,'login.html')
+
+def dashboard(request):
+	return render(request,'warehouse_dashboard.html')
